ccae5b95eda16aa81c2f8fba094df7332c41ba28
MuhammadAhmadd1 <ahmad.james.2315@gmail.com>
2025-03-18 15:54:21 +0500

    Added Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart'; // Using GetX for navigation
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mynotes/notes.dart';
+import 'package:mynotes/screen/splash_screen.dart';
 
 final mytheme = ThemeData(
   useMaterial3: true,
@@ -10,11 +11,20 @@ final mytheme = ThemeData(
   ),
   textTheme: GoogleFonts.latoTextTheme(),
 );
+
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      // Using GetMaterialApp for navigation
+      debugShowCheckedModeBanner: false,
       theme: mytheme,
-      home: Notes(),
-    ),
-  );
+      home: SplashScreen(),
+    );
+  }
 }
---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mynotes/notes.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Navigate to HomeScreen after 5 seconds
+    Timer(Duration(seconds: 5), () {
+      Get.off(() => Notes());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black, // Set splash background color
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 3, // Takes most of the space
+            child: Center(
+              child: Image.asset(
+                'assets/splash.png',
+                width: 300,
+                height: 300,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 40),
+                child: Text(
+                  'AhmadsNotes',
+                  style: GoogleFonts.dancingScript(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
